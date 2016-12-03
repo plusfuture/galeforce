cbbe7e54b46346b594965bba600cb45027e4761c
Will Zhou <will@localhost.localdomain>
2016-12-03 01:28:54 -0500

    commiting code to deploy to i6

--- galeforce/app.js
+++ galeforce/app.js
@@ -68,7 +68,26 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // connect to mongoose
-mongoose.connect('mongodb://localhost/galeforce');
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+if (process.env.NODE_ENV == 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ var fs = require('fs');
+ var path = require('path');
+ var fn = path.join(__dirname, 'config.json');
+ var data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ var conf = JSON.parse(data);
+ var dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/galeforce';
+}
+//mongoose.connect('mongodb://localhost/galeforce');
+mongoose.connect(dbconf);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
